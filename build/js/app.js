36d4e9356604147170644e27d43a0ea8ef7673b8
html5coder <romaalupka@yandex.ru>
2015-01-23 21:33:57 +0300

    del build

--- build/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.main-slider .slides').bxSlider({
-        nextSelector: '.main-slider .next',
-        prevSelector: '.main-slider .prev',
-        nextText: 'Следующая',
-        prevText: 'Предыдущая',
-        mode: 'fade'
-    });
-});
